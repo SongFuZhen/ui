90b44b619309713cd272958ca292a6741fa0cd52
SongFuZhen <evil_song@126.com>
2016-03-07 18:19:23 +0800

    beike wpf

---
+++ Beike/Beike/table.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Beike
+{
+    class table
+    {
+        public string Assembly {get;set;}
+        public string Prod_Line {get;set;}
+        public string Planned {get;set;}
+        public string Produced {get;set;}
+        public string Rest {get;set;}
+        public string Status { get;set;}
+    }
+}
